f022ccd8dfdd992315453028e65b71f033109994
diodeamy <99472600+diodeamy@users.noreply.github.com>
2023-07-20 18:10:43 +0300

    Update energyCallibration.cpp

---
+++ energyCallibration.cpp
@@ -0,0 +1,14 @@
+// ## 3meV?
+// ## extragem arii din zona 
+// ## aliniem un anumit peak which gets shifted left and right because of shifts in temperatue
+// ## n fisiere trebuie alineate cateva peak-uri
+// ## faci un grafic energii prim fisier si un al doilea (E1 vs E2) care os a iti dea exact o linie/polinom cu care sa faci un fit;
+// ## scriptul o sa trb ssa faca asta pt fiecare alt grafic 
+// ## TSpectrum (poate te ajuta sa gasesti peak-uri) "ROOT::TSpectrum in Python" 
+// ## poate merge cu niste hard-coded ranges intre care nu se shiftuiesc peakurile
+// ## poti sa faci alta chestie pt ultimul: mergi din dreapta pana gasesti primul peak
+// ## MakeFile files for easy compilation (" ex g++ *.cpp -std=c++17")
+
+int main() {
+    
+}
