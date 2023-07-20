70f8c09984e7ce166dd45eec7f008ebbe334ec96
diodeamy <99472600+diodeamy@users.noreply.github.com>
2023-07-21 00:05:54 +0300

    Update energyCalibration.cpp
     added part that actually draws the peaks onto the existing histogram; still need to tweak the parameters to get appropriate peakss

--- energyCalibration.cpp
+++ energyCalibration.cpp
@@ -8,6 +8,13 @@
 // ## poate merge cu niste hard-coded ranges intre care nu se shiftuiesc peakurile
 // ## poti sa faci alta chestie pt ultimul: mergi din dreapta pana gasesti primul peak
 // ## MakeFile files for easy compilation (" ex g++ *.cpp -std=c++17")
+#include "TCanvas.h"
+#include "TMath.h"
+#include "TH1.h"
+#include "TF1.h"
+#include "TSpectrum.h"
+#include "TVirtualFitter.h"
+#include <cmath>
 
 void energyCalibration() {
     TFile *f1 = TFile::Open("data/deuterated-digitizer_1.root");
@@ -15,9 +22,12 @@ void energyCalibration() {
     TFile *f3 = TFile::Open("data/deuterated-digitizer_3.root");
 
     TTree *tree1, *tree2, *tree3;
-    UShort_t *detectorId;
-    UShort_t *energy;
-
+    UShort_t detectorId;
+    UShort_t energy;
+    Int_t npeaks = 5;
+    auto binnum = pow(2, 14);
+    auto binmin = 0;
+    auto binmax = pow(2, 14); 
 
     f1->GetObject("events", tree1);
     f2->GetObject("events", tree2);
@@ -29,4 +39,22 @@ void energyCalibration() {
     tree2->SetBranchAddress("detectorId", &detectorId);
     tree2->SetBranchAddress("energy", &energy);
 
+    tree3->SetBranchAddress("detectorId", &detectorId);
+    tree3->SetBranchAddress("energy", &energy);
+    
+    auto nentries1 = tree1->GetEntries();
+
+    TH1D *h1 = new TH1D("h1", "energy", binnum, binmin, binmax);
+    
+    for (int jentry=0; jentry<nentries1; jentry++) {
+        tree1->GetEntry(jentry);
+        if (detectorId == 0) {
+            h1->Fill(energy);
+        }
+    }
+    h1->Draw();
+
+    TSpectrum *s1 = new TSpectrum(2*npeaks);
+    Int_t pfound1 = s1->Search(h1, 2.9, "", 0.10);
+    printf("Found %d candidate peaks to fit \n", pfound1);
 }
