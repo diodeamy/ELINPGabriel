0aa5a12a1680ee17dc58094a5d2241f287464c7e
diodeamy <99472600+diodeamy@users.noreply.github.com>
2023-07-24 17:08:01 +0300

    Create SearchHR12.cpp
    this uses two separate histograms for peak-finding because different parameters were needed for each section of the graph.
    
    This allows more peaks to be found (9) for better calibration.

---
+++ SearchHR12.cpp
@@ -0,0 +1,127 @@
+#include "TString.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TSpectrum.h"
+#include "TPolyMarker.h"
+#include "TTree.h"
+
+
+void SearchHR12() {
+   TTree *tree;
+   UShort_t detectorId;
+   UShort_t energy;
+   Double_t fPositionX[100];
+   Double_t fPositionY[100];
+   Int_t fNPeaks = 0;
+   Int_t i,nfound_1, nfound_2,bin;
+   const Int_t nbins = 4000;
+   Double_t xmin_1 = 0, xmin_2 = 10500; //xmin_1 is for the first 4 peaks and xmin_2 for the last 5
+   Double_t xmax_1= nbins, xmax_2= nbins+xmin_2; 
+   Double_t a;
+   Double_t source[nbins], dest_1[nbins], dest_2[nbins];
+   Double_t sigma_1 = 12, sigma_2 = 19, threshold_1 = 0.8, threshold_2 = 0.9;
+   Int_t deconiterations_1 = 4, deconiterations_2 = 2, averWindow_1 = 2, averWindow_2 = 7;
+
+
+   TFile *f1 = TFile::Open("data/deuterated-digitizer_1.root");
+   f1->GetObject("events", tree);
+
+   tree->SetBranchAddress("energy", &energy);
+   tree->SetBranchAddress("detectorId", &detectorId); 
+
+   Long64_t nentries = tree->GetEntries();
+   
+   //first part stuff
+   TH1F *h_1 = new TH1F("hist1", "energy", nbins, xmin_1, xmax_1);
+    
+   // TH1F *h = (TH1F*) f1->GetObject("events", tree);  //this imports a histogtam from tree; we don't have one
+   for (int jentry=0; jentry<nentries; jentry++) {
+      tree->GetEntry(jentry);
+      if (detectorId == 0) {
+         h_1->Fill(energy);
+        }
+    }
+
+   h_1->SetTitle("High resolution peak searching, number of iterations = 4");
+   h_1->GetXaxis()->SetRange(1,nbins);
+   TH1F *d_1 = new TH1F("d_1","",nbins,xmin_1,xmax_1);
+   h_1->Draw("L");
+   for (i = 0; i < nbins; i++) source[i]=h_1->GetBinContent(i + 1);
+   
+   // h_1->Draw("L");
+ 
+   TSpectrum *s_1 = new TSpectrum();
+ 
+   nfound_1 = s_1->SearchHighRes(source, dest_1, nbins, sigma_1, threshold_1, kTRUE, deconiterations_1, kTRUE, averWindow_1);
+   Double_t *xpeaks_1 = s_1->GetPositionX();
+   for (i = 0; i < nfound_1; i++) {
+      a=xpeaks_1[i];
+      bin = 1 + Int_t(a + 0.5);
+      fPositionX[i] = h_1->GetBinCenter(bin);
+      fPositionY[i] = h_1->GetBinContent(bin);
+   }
+ 
+   TPolyMarker * pm_1 = (TPolyMarker*)h_1->GetListOfFunctions()->FindObject("TPolyMarker");
+   if (pm_1) {
+      h_1->GetListOfFunctions()->Remove(pm_1);
+      delete pm_1;
+   }
+   pm_1 = new TPolyMarker(nfound_1, fPositionX, fPositionY);
+   h_1->GetListOfFunctions()->Add(pm_1);
+   pm_1->SetMarkerStyle(23);
+   pm_1->SetMarkerColor(kRed);
+   pm_1->SetMarkerSize(1.3);
+ 
+   for (i = 0; i < nbins; i++) d_1->SetBinContent(i + 1,dest_1[i]);
+   d_1->SetLineColor(kRed);
+   d_1->Draw("SAME");
+   
+   //second part stuff//
+   TH1F *h_2 = new TH1F("hist2", "energy", nbins, xmin_2, xmax_2);
+
+   for (int jentry=0; jentry<nentries; jentry++) {
+      tree->GetEntry(jentry);
+      if (detectorId == 0) {
+         h_2->Fill(energy);
+        }
+    }
+
+   h_2->SetTitle("High resolution peak searching, number of iterations = 2");
+   h_2->GetXaxis()->SetRange(1,nbins);
+   printf("hello");
+   TH1F *d_2 = new TH1F("d_2","",nbins,xmin_2,xmax_2);
+   h_2->Draw("L");
+   for (i = 0; i < nbins; i++) source[i]=h_2->GetBinContent(i + 1);
+
+
+   TSpectrum *s_2 = new TSpectrum();
+ 
+   nfound_2 = s_2->SearchHighRes(source, dest_2, nbins, sigma_2, threshold_2, kTRUE, deconiterations_2, kTRUE, averWindow_2);
+   Double_t *xpeaks_2 = s_2->GetPositionX();
+   for (i = 0; i < nfound_2; i++) {
+      a=xpeaks_2[i];
+      bin = 1 + Int_t(a + 0.5);
+      fPositionX[i] = h_2->GetBinCenter(bin);
+      fPositionY[i] = h_2->GetBinContent(bin);
+   }
+ 
+   TPolyMarker * pm_2 = (TPolyMarker*)h_2->GetListOfFunctions()->FindObject("TPolyMarker");
+   if (pm_2) {
+      h_2->GetListOfFunctions()->Remove(pm_2);
+      delete pm_2;
+   }
+   pm_2 = new TPolyMarker(nfound_2, fPositionX, fPositionY);
+   h_2->GetListOfFunctions()->Add(pm_2);
+   pm_2->SetMarkerStyle(23);
+   pm_2->SetMarkerColor(kRed);
+   pm_2->SetMarkerSize(1.3);
+ 
+   for (i = 0; i < nbins; i++) d_2->SetBinContent(i + 1,dest_2[i]);
+   d_2->SetLineColor(kRed);
+   d_2->Draw("SAME");
+ 
+   printf("Found %d candidate peaks in first part\n and %d in second part \n",nfound_1, nfound_2);
+   for( i=0;i<nfound_1;i++) printf("posx= %f, posy= %f\n",fPositionX[i], fPositionY[i]);
+   for (i=0;i<nfound_2;i++) printf("posx = %f, posy = %f\n", fPositionX[i], fPositionY[i]);
+
+}
